8b2927ca8eea7d71eecfe29e3956546a76c1b8dd
betzadi.vega@gmail.com <desarrollo.dos.cbi@gmail.com>
2023-07-11 14:42:19 -0600

    implementacion DNA

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,7 @@ const deploy = async () => {
   console.log("Deploying contract with the account:", deployer.address);
  
   const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
-  const deployed = await PlatziPunks.deploy(3);
+  const deployed = await PlatziPunks.deploy(2);
  
   console.log("Platzi Punks is deployed at:", deployed.target);
 };
